2f8987c2770ad0e3647cd5eb9240f03e3a38306c
Александр Самарин <a.samarin@atol.ru>
2026-05-11 18:15:17 +0300

    api_key multi filter

--- src/api/v1/endpoints/options.py
+++ src/api/v1/endpoints/options.py
@@ -98,5 +98,5 @@ async def get_api_key_options(
 
     return [
         {'text': api_key, 'value': api_key}
-        for api_key in list(api_keys) if api_key
+        for api_key in sorted(list(api_keys)) if api_key
     ]
